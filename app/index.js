692408355faccaf4fc519c96eeb4c8b5c8d5c21c
briandhkim <w1057216@gmail.com>
2017-11-07 11:15:45 -0800

    don't remember the change

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import $ from 'jquery';
+import * as math from './math';
+
+$('#root').append('<h2> added with $</h2>');
+
+$('#root').append(`<h3> 6 + 9 = ${math.add(6,9)}</h3>`);
+$('#root').append(`<h3> 9 - 3 = ${math.subtract(9,3)} </h3>`);
+$('#root').append(`<h3> 9 / 3 = ${math.div(9,3)} </h3>`);
+$('#root').append(`<h3> 8 x 3 = ${math.mult(8,3)} </h3>`);
+$('#root').append(`<h3> 4 / 0 = ${math.div(4,0)} </h3>`);
+
+console.log(`math object : ${math}`, math);
---
+++ app/math.js
@@ -0,0 +1,9 @@
+export function add(x, y){
+	return x+y;
+}
+
+export const subtract = (x,y) => x - y;
+
+export const mult = (x,y) => x*y;
+
+export const div = (x,y) => y ? x/y : 'Can\'t divide by zero';
